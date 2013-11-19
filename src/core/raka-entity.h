d9f926793e42327b0bb3785a9faeaa1d7f1ca635
Tim O'Brien (desktop) <tsob@ccrma.stanford.edu>
2013-11-18 21:16:46 -0800

    updating raka-entity; removing superfluous stuff

--- src/core/raka-entity.h
+++ src/core/raka-entity.h
@@ -12,43 +12,6 @@
 #include "x-buffer.h"
 #include <vector>
 
-
-
-
-//-----------------------------------------------------------------------------
-// name: class RAKASpark
-// desc: ...
-//-----------------------------------------------------------------------------
-class RAKASpark : public YEntity
-{
-public:
-    // constructor
-    RAKASpark() : texture(0), ALPHA( 1, 1, 1 ) { }
-    
-public:
-    // set
-    void set( int _texture, float _size, float _alpha );
-    void setTexture( int _texture );
-    void setSize( float _size );
-    
-public:
-    // update
-    void update( YTimeInterval dt );
-    // render
-    void render();
-    
-public:
-    // which ripple texture
-    GLuint texture;
-    // vertices
-    GLfloat vertices[8];
-    // alpha ramp
-    Vector3D ALPHA;
-};
-
-
-
-
 //-----------------------------------------------------------------------------
 // name: class RAKATeapot
 // desc: for testing
@@ -61,9 +24,6 @@ public:
     // render
     void render();
 };
-
-
-
 
 #endif
 
