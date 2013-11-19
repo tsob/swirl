8dacabaa1afcad328ce5ef8b60bd2fe56615d09e
Tim O'Brien (desktop) <tsob@ccrma.stanford.edu>
2013-11-18 21:36:59 -0800

    add top comments to raka-audio

---
+++ src/core/raka-audio.cpp
@@ -0,0 +1,9 @@
+//-----------------------------------------------------------------------------
+// Name: raka-audio.cpp
+// Desc: audio processing
+//
+// Author: Tim O'Brien    (tsob@ccrma.stanford.edu)
+//         Reza Payami  (rpayami@ccrma.stanford.edu)
+//         Haley Sayres    (hsayres@stanford.edu)
+// Date:   Fall 2013
+//-----------------------------------------------------------------------------
